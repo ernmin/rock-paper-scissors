362ac50ff3b6e4aadf86f0b28f9460002a503c74
ernmin <42770608+ernmin@users.noreply.github.com>
2024-06-06 19:19:28 +0800

    Write playRound function Check first for a tie Check using nested if else depending on human's input

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 function getComputerChoice(){
     let num = Math.random();
     if (num >= 0 && num < 0.33){
-        return 'Rock';
+        return 'rock';
     }
     else if (num >= 0.33 && num < 0.67){
-        return 'Paper';
+        return 'paper';
     }
     else{
-        return 'Scissors';
+        return 'scissors';
     }
 
 }
@@ -24,5 +24,35 @@ let computerScore = parseInt(0);
 //Write a function called playRound that uses computer's choice and player's choice as arguments
     //Function should output the winner
     //humanChoice should be case insensitive
+function playRound(human, computer) {
+    if (human === computer){
+        return 'Tie'
+    }
+    else if (human === 'Rock') {
+        if(computer === 'Paper'){
+            return 'You lose!'
+        }
+        else if (computer === 'Scissors'){
+            return 'You win!'
+        }
+    }
+    else if (human === 'Paper'){
+        if (computer === 'Rock'){
+            return 'You win!'
+        }
+        else if (computer === 'Scissors'){
+            return 'You lose!'
+        }
+    }
+    else if (human === 'Scissors'){
+        if (computer === 'Paper'){
+            return 'You win!'
+        }
+        else if (computer === 'Rock'){
+            return 'You lose!'
+        }
+    }
+}
 
+console.log(playRound('Scissors', 'Paper'))
 //Write a function called playGame that calls playRound to play 5 rounds
